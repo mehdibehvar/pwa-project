db4f968f205beeffb6f794591a5090bba1573ae5
mehdibehvar <mehdibehvar00@gmail.com>
2022-10-24 16:20:47 +0330

    fire-install-prompt-by-click-button

--- public/app.js
+++ public/app.js
@@ -1,3 +1,24 @@
+const fab=document.querySelector("#fab");
+var beforeinstallpromptEvent;
+fab.addEventListener("click",function () {
+    if (beforeinstallpromptEvent) {
+        beforeinstallpromptEvent.prompt("do you want install awesome note app?");
+        beforeinstallpromptEvent.userChoice.then(function (choice) {
+            if (choice.outcome==="dismissed") {
+                console.log(choice);
+            }else{
+                console.log(choice);
+            }
+        })
+        //we null the beforeinstallpromptEvent to beforeinstallprompt don't execute again.just execute one time;
+        beforeinstallpromptEvent=null;
+    }
+})
+window.addEventListener("beforeinstallprompt",function (event) {
+    console.log(event);
+    event.preventDefault();
+    beforeinstallpromptEvent=event;
+})
 if ("serviceWorker" in navigator) {
     window.addEventListener("load",function () {
         navigator.serviceWorker.register("sw.js").then(function () {
--- public/sw.js
+++ public/sw.js
@@ -1,15 +1,18 @@
 self.addEventListener("install",function (event) {
     console.log("service worker sugest installing...",event);
+    //if we skipwaiting then the sw will active and start running//
     // self.skipWaiting();
 })
 self.addEventListener("activate",function (event) {
-    console.log("service worker is activated",event);
+
+    console.log("service worker is activated and is running",event);
 })
 self.addEventListener("fetch",function (event) {
-    console.log("service worker is fetching...",event);
+    console.log("[Service Worker] Fetched resource",event);
 })
 
-
+///وقتی ما یک سری تغیرات را در سرویس ورکر حود ایجاد میکنیم.این سرویس ورکر جدید و اپدیت شده وارد حالت انتظار میشوود و تا زمانی که ما اسکیپ ویتینگ ..نکنیم (چه با استفاده از developer tools or self.skipWaiting()) .سرویس ورکر جدید اکتیو نمیشود و شروع به کار نمیکند.حتی اگر صفحه را رفرش کنیم.
+//نکته : اگر بعد از اکسپ کردن در دولوپر تولز صفخه را رفرش نکنیم تغیرات جدید اعمال نمیشه و سرویس ورکر جدید ران نمیشه.ولی اگر از تابع اسکیپ ویتینگ در کد استفاده کنیم دیکر نیاز نیست و وقتی کاربر به اپلیکیشن ما درخواست میزند به صورت اتومات اپدیت میشه.
 
 
 
